16ed5842254b4109a8136279c2cad0a7ea12eb3a
shuntacreative <shuntacreative@gmail.com>
2021-06-15 12:24:01 +0900

    useモデル

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :installs
+  
+
+  devise_for :users
   get 'dreamys/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "dreamys#index"
